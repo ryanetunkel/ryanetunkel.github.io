85046a5f84c9c259583e93bfc8a71c7085b40547
ryanetunkel <123666075+ryanetunkel@users.noreply.github.com>
2023-04-22 12:09:58 -0400

    More JS work

--- script.js
+++ script.js
@@ -23,7 +23,20 @@ async function mainEvent() {
   const loadDataButton = document.querySelector("#data_load");
   const clearDataButton = document.querySelector("#data_clear");
   const generateListButton = document.querySelector("#generate");
-  const textField = document.querySelector("#resto");
+  const textField = document.querySelector("#text_field");
+  const generalSearchFilter = document.querySelector("#general_search");
+  const armorFilter = document.querySelector("#armor");
+  const backgroundsFilter = document.querySelector("#backgrounds");
+  const classesFilter = document.querySelector("#classes");
+  const conditionsFilter = document.querySelector("#conditions");
+  const featsFilter = document.querySelector("#feats");
+  const magicItemsFilter = document.querySelector("#magic_items");
+  const monstersFilter = document.querySelector("#monsters");
+  const racesFilter = document.querySelector("#races");
+  const spellsFilter = document.querySelector("#spells");
+  const weaponsFilter = document.querySelector("#weapons");
+  const apiSite = "https://api.open5e.com/";
+  let searchTerm = "";
   
   generateListButton.classList.add("hidden");
 
@@ -41,8 +54,8 @@ async function mainEvent() {
     console.log("Loading data"); // this is substituting for a "breakpoint" - it prints to the browser to tell us we successfully submitted the form
 
     // Basic GET request - this replaces the form Action
-    const results = await fetch(
-      "https://api.open5e.com"
+    let results = await fetch(
+      apiSite.concat(searchTerm)
     );
 
     // This changes the response from the GET into data we can use - an "object"
@@ -53,8 +66,6 @@ async function mainEvent() {
     if (parsedData?.length > 0) { 
       generateListButton.classList.remove("hidden");
     }
-
-    // console.table(storedList); 
   });
 
   generateListButton.addEventListener("click", (event) => {
@@ -74,6 +85,61 @@ async function mainEvent() {
     console.log("clear browser data");
     localStorage.clear();
     console.log("localStorage Check", localStorage.getItem("storedData"));
+  });
+
+  generalSearchFilter.addEventListener("click", (event) => {
+    console.log("general search filter");
+    searchTerm = "search";
+  });
+
+  armorFilter.addEventListener("click", (event) => {
+    console.log("armor filter");
+    searchTerm = "armor";
+  });
+
+  backgroundsFilter.addEventListener("click", (event) => {
+    console.log("backgrounds filter");
+    searchTerm = "backgrounds";
+  });
+
+  classesFilter.addEventListener("click", (event) => {
+    console.log("classes filter");
+    searchTerm = "classes";
+  });
+
+  conditionsFilter.addEventListener("click", (event) => {
+    console.log("conditions filter");
+    searchTerm = "conditions";
+  });
+
+  featsFilter.addEventListener("click", (event) => {
+    console.log("feats filter");
+    searchTerm = "feats";
+  });
+
+  magicItemsFilter.addEventListener("click", (event) => {
+    console.log("magic items filter");
+    searchTerm = "magicitems";
+  });
+
+  monstersFilter.addEventListener("click", (event) => {
+    console.log("monsters filter");
+    searchTerm = "monsters";
+  });
+
+  racesFilter.addEventListener("click", (event) => {
+    console.log("races filter");
+    searchTerm = "races";
+  });
+
+  spellsFilter.addEventListener("click", (event) => {
+    console.log("spells filter");
+    searchTerm = "spells";
+  });
+
+  weaponsFilter.addEventListener("click", (event) => {
+    console.log("weapons filter");
+    searchTerm = "weapons";
   });
 }
 
